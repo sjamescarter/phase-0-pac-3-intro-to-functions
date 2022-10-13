75a2baf233b4d2ef46221d78f663aa7f8cbdd7a4
sjamescarter <stephenjamescarter@gmail.com>
2022-10-12 21:53:18 -0500

    commit tree 84e84829465f90fc95e31175ebbec7983a279dd5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Follow along with the examples here
+function add(x, y) {
+    return x + y;
+}
+
+if(add(2, 1.8) >6) {
+    console.log(`${add(2, 1.8)} is greater than 6.`);
+} else {
+    console.log(`${add(2, 1.8)} is less than 6.`);
+}  
